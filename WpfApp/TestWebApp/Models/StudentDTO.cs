c7764cf23fb2f6b194383c6aa9588c02d58b6446
Humza Tufail <humza.tufail@confiz.com>
2018-08-20 18:44:20 +0500

    Some important Changes

---
+++ WpfApp/TestWebApp/Models/StudentDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Models
+{
+    /// <summary>
+    /// Class Contains the Student Data
+    /// </summary>
+    public class StudentDTO
+    {
+        /// <summary>
+        /// Name Property of the Student
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// Last name Property of the Student
+        /// </summary>
+        public String Lastname { get; set; }
+
+        /// <summary>
+        /// Gender of the Student
+        /// </summary>
+        public String  Gender { get; set; }
+
+        /// <summary>
+        /// CNIC of the Student
+        /// </summary>
+        public String CNIC { get; set; }
+
+        /// <summary>
+        /// This method init the students DTO
+        /// </summary>
+        public void Init()
+        {
+            Debug.WriteLine("Init Method");
+        }
+    }
+}
